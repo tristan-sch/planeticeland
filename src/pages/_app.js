469cbf76b03a32cb4cb143872604f14d66110804
tristan-sch <schmale.tristan@gmail.com>
2023-01-15 15:53:04 +0000

    added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import { GlobalStyles } from "twin.macro";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
