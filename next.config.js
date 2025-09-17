36f463df3ad93c0f682a4a245302631dbfd7abaa
tristan-sch <schmale.tristan@gmail.com>
2025-09-17 20:43:36 +0000

    runned auto imports on missed files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["wp.planeticeland.is", "images.unsplash.com"],
+    domains: ['wp.planeticeland.is', 'images.unsplash.com'],
   },
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
